90ddee3f14cd7741df90780ce57fa4d041cc201d
Kristiyan Stoimenov <kristoimenov@gmail.com>
2021-07-01 13:47:56 +0300

    Helper functions: helping, max_phase

--- src/tests/TestHelpQueue.cc
+++ src/tests/TestHelpQueue.cc
@@ -3,8 +3,26 @@
 
 using namespace helpqueue;
 
-TEST(HelpQ, Construction) {
-  auto hq = HelpQueue<int>{};
+namespace helpqueue_testsuite {
+
+class HelpQueueTest : public ::testing::Test {
+ protected:
+  HelpQueue<int> hq{};
+
+  void SetUp() override {}
+
+  void TearDown() override {}
+};
+
+TEST_F(HelpQueueTest, PeekNoValue) {
   auto head = hq.peek();
   EXPECT_TRUE(!head.has_value());
 }
+
+TEST_F(HelpQueueTest, MaxPhaseNoValues) {
+//  auto max = hq.max_phase();
+//  EXPECT_TRUE(!max.has_value());
+  EXPECT_TRUE(false);
+}
+
+} // helpqueue_testsuite
