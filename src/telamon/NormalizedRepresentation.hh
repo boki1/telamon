682f203ad4b4349f7bd3490da2e5fa073235fb07
Kristiyan Stoimenov <kristoimenov@gmail.com>
2021-07-05 17:30:51 +0300

    Define structures related to operation records

--- src/telamon/NormalizedRepresentation.hh
+++ src/telamon/NormalizedRepresentation.hh
@@ -15,21 +15,10 @@
 #include <utility>
 #include <optional>
 
-//#include <folly/Expected.h>
-//template<typename Value, typename Error>
-//using expected = folly::Expected<Value, Error>;
-
 #include <extern/expected_lite/expected.hpp>
-using namespace nonstd;
-
-#include "HelpQueue.hh"
-#include "OperationHelping.hh"
 
 namespace telamon_simulator {
 
-class NormalizedRepresentation {};
-
-///
 /// \brief Measures the contention which was encountered during simulation
 /// \details Keeps an internal counter of the detected contention and responds according to it.
 class ContentionMeasure {
@@ -51,85 +40,17 @@ class ContentionMeasure {
 /// \details The`generator` and `wrap_up` functions correspond to the first and third stage of the algorithm operation.
 /// 		 The fast path represents the steps which are used when the operation in executed as lock-free.
 template<typename LockFree>
-concept NormalizedLockFree = requires(LockFree lf, ContentionMeasure &contention, const typename LockFree::Input &inp,
+concept NormalizedRepresentation = requires(LockFree lf, ContentionMeasure &contention, const typename LockFree::Input &inp,
 									  const typename LockFree::CommitDescriptor &desc, std::optional<int> executed) {
   typename LockFree::Input;
   typename LockFree::Output;
   typename LockFree::CommitDescriptor;
 
-  { lf.wrap_up(inp, contention) } -> std::same_as<expected<std::optional<typename LockFree::Output>, ContentionMeasure>>;
+  { lf.wrap_up(inp, contention) } -> std::same_as<nonstd::expected<std::optional<typename LockFree::Output>, ContentionMeasure>>;
 
-  { lf.generator(desc, executed, contention) } -> std::same_as<expected<typename LockFree::Input, typename LockFree::Output>>;
+  { lf.generator(desc, executed, contention) } -> std::same_as<nonstd::expected<typename LockFree::Input, typename LockFree::Output>>;
 
-  { lf.fast_path(inp, contention) } -> std::same_as<expected<typename LockFree::Output, ContentionMeasure>>;
-};
-
-template<typename LockFree, const int N = 16, typename = std::enable_if<(N > 0)>> requires NormalizedLockFree<LockFree>
-class WaitFreeSimulatorHandle {
- public:
-  using Id_type = int;
- private:
-  struct Common {
-	LockFree m_algorithm;
-	helpqueue::HelpQueue<LockFree, N> m_helpqueue;
-	std::vector<Id_type> m_free;
-	std::mutex m_free_lock;
-
-	explicit Common(const LockFree &lf) : m_algorithm{lf}, m_helpqueue{} {
-	  m_free.resize(N - 1);
-	  std::iota(m_free.begin(), m_free.end(), 1);
-	}
-
-	explicit Common(LockFree &&lf) : m_algorithm{std::move(lf)}, m_helpqueue{} {
-	  m_free.resize(N);
-	  std::generate(m_free.begin(), m_free.end(), [n = 0]() mutable { return n++; });
-	}
-  };
-
- public: //< Construction API
-  explicit WaitFreeSimulatorHandle(LockFree algorithm)
-	  : m_id{0}, m_common_data{std::make_shared<Common>(algorithm)} {
-  }
-
-  // TODO: Check notes
-  auto Fork() -> std::optional<WaitFreeSimulatorHandle<LockFree, N>> {
-	auto lock = std::lock_guard<std::mutex>{m_common_data->m_free_lock};
-	if (m_common_data->m_free.empty()) {
-	  return {};
-	}
-	auto next_id = m_common_data->m_free.back();
-	m_common_data->m_free.pop_back();
-	return WaitFreeSimulatorHandle{next_id, m_common_data};
-  }
-
-  /// TODO: Should be moved to destructor, but the id is retired in other situations as well - thus, the additional function.
-  void Retire() {
-	const auto lock = std::lock_guard{m_common_data->m_free_lock};
-	m_common_data->m_free.push_back(m_id);
-  }
-
- public: //< Perform operation API
-
-  /// \brief Runs the actual simulation
-  /// \param input The given input
-  /// \return The output of the operation
-  auto simulate(const typename LockFree::Input &input) -> typename LockFree::Output {}
-
-  /// \brief Checks whether other threads need help with a certain operation and tries to help them
-  auto help() -> void {}
-
-  /// \brief Helps a specific operation
-  /// \note After exiting this function the operation encapsulation in `op_box` will be completed
-  /// \param op_box	The operation box containing a ptr to the operation which requires help
-  auto help(const OperationRecordBox &op_box) -> void {}
-
- private:
-  WaitFreeSimulatorHandle(Id_type id, std::shared_ptr<Common> common_ptr)
-	  : m_common_data{common_ptr}, m_id{id} {}
-
- private:
-  std::shared_ptr<Common> m_common_data;
-  Id_type m_id;
+  { lf.fast_path(inp, contention) } -> std::same_as<nonstd::expected<typename LockFree::Output, ContentionMeasure>>;
 };
 
 //!
