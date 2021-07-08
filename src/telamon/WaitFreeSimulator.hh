1694a3c6ba729fdafda12494da4f4dc4ea051242
Kristiyan Stoimenov <kristoimenov@gmail.com>
2021-07-08 14:59:36 +0300

    Refactor simulator hierarchy

--- src/telamon/WaitFreeSimulator.hh
+++ src/telamon/WaitFreeSimulator.hh
@@ -26,8 +26,7 @@ struct OverloadedVisitor : T ... { using T::operator()...; };
 namespace telamon_simulator {
 
 template<NormalizedRepresentation LockFree, const int N = 16>
-class WaitFreeSimulatorHandle {
- public:
+class WaitFreeSimulator {
   using Id = int;
   using Input = typename LockFree::Input;
   using Output = typename LockFree::Output;
@@ -40,33 +39,12 @@ class WaitFreeSimulatorHandle {
   template<typename T, typename Err = std::monostate>
   using OptionalResultOrError = nonstd::expected<std::optional<T>, Err>;
 
- private:
-  /// \brief Encapsulates the data shared between all simulator handles
-  struct MetaData;
+ public:
+  explicit WaitFreeSimulator (const LockFree &lf) : m_algorithm{lf}, m_helpqueue{} {}
 
- public: //< Construction API
-  explicit WaitFreeSimulatorHandle (LockFree algorithm)
-	  : m_id{0}, m_meta{std::make_shared<MetaData>(algorithm)} {
-	  static_assert(N > 0, "N has to be a positive integer.");
-  }
+  explicit WaitFreeSimulator (LockFree &&lf) : m_algorithm{std::move(lf)}, m_helpqueue{} {}
 
-  auto fork () -> std::optional<WaitFreeSimulatorHandle<LockFree, N>> {
-	  const auto lock = std::lock_guard<std::mutex>{m_meta->m_free_lock};
-	  if (m_meta->m_free.empty()) {
-		  return {};
-	  }
-	  auto next_id = m_meta->m_free.back();
-	  m_meta->m_free.pop_back();
-	  return WaitFreeSimulatorHandle{next_id, m_meta};
-  }
-
-  /// TODO: Check notes. Should be moved to destructor, but the id is retired in other situations as well - thus, the additional function.
-  void retire () {
-	  const auto lock = std::lock_guard{m_meta->m_free_lock};
-	  m_meta->m_free.push_back(m_id);
-  }
-
- public: //< Perform operation API
+ public:
 
   /// \brief 	Runs the actual simulation
   /// \param 	input The given input
@@ -74,7 +52,7 @@ class WaitFreeSimulatorHandle {
   ///           the contention threshold is reached, the fast-path is abandoned and the operation is switched to the slow-path, which asks the other
   /// 			executing threads for help.
   /// \return 	The output of the operation
-  auto simulate (const Input &input) -> Output {
+  auto run (const Id id, const Input &input) -> Output {
 	  auto contention_counter = ContentionFailureCounter{};
 	  try_help_others();
 
@@ -83,31 +61,29 @@ class WaitFreeSimulatorHandle {
 		  if (fp_result.has_value()) {
 			  return fp_result.value();
 		  }
-
 		  if (contention_counter.detect()) {
 			  break;
 		  }
 	  }
 
-	  return slow_path(input);
+	  return slow_path(id, input);
   }
 
   /// \brief 	Checks whether other threads need help with a certain operation and tries to help them
-  auto try_help_others () -> void {
-	  auto meta = atomic_load(&m_meta);
-	  auto front = meta->m_helpqueue.peek_front();
+  auto try_help_others (const Id id) -> void {
+	  auto front = m_helpqueue.peek_front();
 	  if (front.has_value()) {
 		  help(front.value());
 	  }
   }
 
- private: //< Internal operations
+ private:
+  /// \brief	Helps an operation in the precas stage
   auto help_precas (OpBox &op_box, const OpRecord &op, const typename OpRecord::PreCas &state) -> OptionalResultOrError<OpRecord *> {
-	  auto meta = atomic_load(&m_meta);
 	  auto failures = ContentionFailureCounter{};
 
 	  // Generate CAS-list
-	  auto desc = meta->m_algorithm.generator(op.input(), failures);
+	  auto desc = m_algorithm.generator(op.input(), failures);
 	  if (!desc.has_value()) {
 		  return nonstd::make_unexpected(std::monostate{});
 	  }
@@ -116,11 +92,11 @@ class WaitFreeSimulatorHandle {
 	  return new OpRecord{op, updated_state};
   }
 
+  /// \brief	Helps an operation in the postcas stage
   auto help_postcas (OpBox &op_box, const OpRecord &op, const typename OpRecord::PostCas &state) -> OptionalResultOrError<OpRecord *> {
-	  auto meta = atomic_load(&m_meta);
 	  auto failures = ContentionFailureCounter{};
 
-	  auto result_opt = meta->m_algorithm.wrap_up(state.executed, state.cas_list, failures);
+	  auto result_opt = m_algorithm.wrap_up(state.executed, state.cas_list, failures);
 	  if (!result_opt.has_value()) {
 		  // Contention encountered. Try again.
 		  return nonstd::make_unexpected(std::monostate{});
@@ -137,6 +113,7 @@ class WaitFreeSimulatorHandle {
 	  return new OpRecord{op, updated_state};
   }
 
+  /// \brief	Helps an operation in the stage during cas execution
   auto help_executingcas (OpBox &op_box, const OpRecord &op, typename OpRecord::ExecutingCas &state) -> OptionalResultOrError<OpRecord *, int> {
 	  auto failures = ContentionFailureCounter{};
 
@@ -193,8 +170,7 @@ class WaitFreeSimulatorHandle {
 				return std::make_pair(continue_, unit);
 			  },
 			  [&] (const typename OpRecord::Completed &arg) -> HelperVisitResult {
-				auto meta = atomic_load(&m_meta);
-				meta->m_helpqueue.try_pop_front(op_box);
+				m_helpqueue.try_pop_front(op_box);
 				return HelperVisitResult{};
 			  }
 		  }, op.state());
@@ -212,7 +188,7 @@ class WaitFreeSimulatorHandle {
   }
 
 /// \brief 	Make progress on each of the CAS-es required by the specific operation based on their state
-/// \param 	cas_list List of the CAS-es required by the specific operation
+/// \param 	cas_list List oreturn f the CAS-es required by the specific operation
 /// \return 	Either a success or an error:
 /// 				Success => The CAS was/were performed successfully
 /// 				Error => Either there was contention during the CAS execution, or the CAS failed (the params were incorrect)
@@ -245,14 +221,13 @@ class WaitFreeSimulatorHandle {
 	  return std::monostate{};
   }
 
-/// \brief 	The slow-path
+/// \brief 		The slow-path
 /// \details 	The slow-path begins as the thread-owner of the operation enqueues a succinct description of the operation it has failed to complete
 /// 			in the fast path (an OperationRecordBox).
-  auto slow_path (const Input &input) -> Output {
+  auto slow_path (const Id id, const Input &input) -> Output {
 	  // Enqueue description of the operation
-	  auto meta = atomic_load(&m_meta);
 	  auto op_box = OperationRecordBox<LockFree>{};
-	  meta->m_helpqueue.push_back(m_id, op_box);
+	  m_helpqueue.push_back(id, op_box);
 
 	  // Help until operation is complete
 	  using StateCompleted = typename OperationRecord<LockFree>::Completed;
@@ -268,36 +243,81 @@ class WaitFreeSimulatorHandle {
 
 /// \brief The fast-path. Directly invokes the fast_path of the algorithm being executed
   auto fast_path (const Input &input, ContentionFailureCounter &contention_counter) -> nonstd::expected<Output, ContentionFailureCounter> {
-	  auto meta = atomic_load(&m_meta);
-	  return meta->m_algorithm.fast_path(input, contention_counter);
+	  return m_algorithm.fast_path(input, contention_counter);
   }
 
  private:
-  WaitFreeSimulatorHandle (Id id, std::shared_ptr<MetaData> common_ptr)
-	  : m_meta{common_ptr}, m_id{id} {}
-
- private:
-  std::shared_ptr<MetaData> m_meta{};
-  Id m_id;
+  LockFree m_algorithm;
+  helpqueue::HelpQueue<OperationRecordBox<LockFree>, N> m_helpqueue;
 };
 
-template<NormalizedRepresentation LockFree, const int N>
-struct WaitFreeSimulatorHandle<LockFree, N>::MetaData {
-  LockFree m_algorithm;
-  helpqueue::HelpQueue<OperationRecordBox<LockFree>, N>
-	  m_helpqueue;
-  std::vector<Id> m_free;
-  std::mutex m_free_lock;
+template<NormalizedRepresentation LockFree, const int N = 16>
+class WaitFreeSimulatorHandle {
+ public:
+  using Id = int;
+  using Input = typename LockFree::Input;
+  using Output = typename LockFree::Output;
+  using CommitDescriptor = typename LockFree::CommitDescriptor;
 
-  explicit MetaData (const LockFree &lf) : m_algorithm{lf}, m_helpqueue{} {
-	  m_free.resize(N - 1);
-	  std::iota(m_free.begin(), m_free.end(), 1);
+  using OpRecord = OperationRecord<LockFree>;
+  using OpBox = OperationRecordBox<LockFree>;
+  using OpState = typename OperationRecord<LockFree>::OperationState;
+
+  template<typename T, typename Err = std::monostate>
+  using OptionalResultOrError = nonstd::expected<std::optional<T>, Err>;
+
+  using Simulator = WaitFreeSimulator<LockFree, N>;
+
+ public:
+  struct MetaData {
+	std::vector<Id> m_free;
+	std::mutex m_free_lock;
+  };
+
+ public: //< Construction API
+  explicit WaitFreeSimulatorHandle (LockFree algorithm)
+	  : m_id{0}, m_simulator{std::make_shared<Simulator>(algorithm)}, m_meta{std::make_shared<MetaData>()} {
+	  // Safe to access m_meta without atomic load because it has never been shared
+	  m_meta->m_free.resize(N - 1);
+	  std::iota(m_meta->m_free.begin(), m_meta->m_free.end(), 1);
+	  static_assert(N > 0, "N has to be a positive integer.");
   }
 
-  explicit MetaData (LockFree &&lf) : m_algorithm{std::move(lf)}, m_helpqueue{} {
-	  m_free.resize(N);
-	  std::generate(m_free.begin(), m_free.end(), [n = 0] () mutable { return n++; });
+  auto fork () -> std::optional<WaitFreeSimulatorHandle<LockFree, N>> {
+	  auto meta = std::atomic_load(&m_meta);
+	  const auto lock = std::lock_guard<std::mutex>{meta->m_free_lock};
+	  if (meta->m_free.empty()) {
+		  return {};
+	  }
+	  auto next_id = meta->m_free.back();
+	  meta->m_free.pop_back();
+	  return WaitFreeSimulatorHandle{next_id, m_simulator, meta};
   }
+
+  auto retire () -> void {
+	  auto meta = std::atomic_load(&m_meta);
+	  const auto lock = std::lock_guard{meta->m_free_lock};
+	  meta->m_free.push_back(m_id);
+  }
+
+  auto submit (const Input &input) -> Output {
+	  auto sim = std::atomic_load(&m_simulator);
+	  return sim->run(m_id, input);
+  }
+
+  auto help () -> void {
+	  auto sim = std::atomic_load(&m_simulator);
+	  sim->try_help_others(m_id);
+  }
+
+ private:
+  WaitFreeSimulatorHandle (Id id, std::shared_ptr<Simulator> t_simulator, std::shared_ptr<MetaData> t_meta)
+	  : m_simulator{t_simulator}, m_id{id}, m_meta{t_meta} {}
+
+ private:
+  std::shared_ptr<Simulator> m_simulator{};
+  std::shared_ptr<MetaData> m_meta{};
+  Id m_id;
 };
 
 }
